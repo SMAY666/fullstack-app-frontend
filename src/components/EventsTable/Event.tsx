233868b45e1dfa35bc7cf23b03ec2b1975d3f8de
Matvey <72608908+SMAY666@users.noreply.github.com>
2023-03-27 02:01:56 +0700

    fix: Edit event border color

--- src/components/EventsTable/Event.tsx
+++ src/components/EventsTable/Event.tsx
@@ -38,7 +38,7 @@ export default function Event({event}: Props) {
             });
     };
     return (
-        <tr className={new Date(event.dateEnd).getTime() - Date.now() < 864000000 ? 'border-t-2 text-center bg-red-200 hover:border-2 hover:border-blue-300 hover:bg-gray-100 hover:text-black duration-300' : 'border-t-2 text-center hover:border-2 hover:border-blue-300 hover:bg-gray-100 duration-300'}>
+        <tr className={new Date(event.dateEnd).getTime() - Date.now() < 864000000 ? 'border-t-2 text-center bg-red-200 hover:border-2 hover:border-red-600 hover:bg-gray-100 hover:text-black duration-300' : 'border-t-2 text-center hover:border-2 hover:border-blue-300 hover:bg-gray-100 duration-300'}>
             <td className="border-r-[1px] py-[10px]">{event.title}</td>
             <td className={event.description.length >= 30 ? 'w-[200px] border-r-[1px] py-[10px] px-[10px] text-justify text-xs' : 'w-[200px] border-r-[1px] py-[10px]'}>{event.description}</td>
             <td className="border-r-[1px] py-[10px]">{new Date(event.dateBegin).toLocaleDateString()}</td>
